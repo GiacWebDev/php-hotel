af997134f65205b5a870df39d0192ba3f15c6fa9
GiacWebDev <giacobechini@gmail.com>
2023-10-26 18:09:41 +0200

    [edit] table bootstrap

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     ],
   ];
 
-  var_dump($hotels)
+  // var_dump($hotels)
   ?>
 
 <!DOCTYPE html>
@@ -46,26 +46,42 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- BOOTSTRAP -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 <title>PHP Hotel</title>
 </head>
 
 <body>
 
-  <?php foreach ($hotels as $hotel): ?>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col">
 
-    <ul>
-      <?php foreach ($hotel as $key => $value): ?>
+      <table class="table">
+        <thead>
+          <tr>
+          <?php foreach(array_keys($hotels[0]) as $header): ?>
+            <th scope="col"><?php echo strtoupper(str_replace('_', ' ', $header)); ?></th>
+          <?php endforeach; ?>
+        </thead>
 
-      <li>
-        <!-- Se la chiave non è "parking allora mostra il valore originale" -->
-        <!-- se è parking se è true = si se è false = no (true / false sono prensenti gia in array-->
-        <?php echo str_replace('_', ' ', $key) . ': ' . ($key === 'parking' ? ($value ? 'Si' : 'No') : $value);?>
-      </li>
+        <tbody>
+          <?php foreach ($hotels as $hotel): ?>
+            <tr>
+              <?php foreach ($hotel as $key => $value): ?>
+                <td><?php echo($key === 'parking' ? ($value ? 'Si' : 'No') : $value);?></td>
+              <?php endforeach; ?>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
 
-      <?php endforeach; ?>
-    </ul>
+      </div>
+    </div>
+  </div>
 
-  <?php endforeach; ?>
+  
 
 </body>
 </html>
